77a379e147d0a717651fe9f9277609e08c96cf1b
owais6153 <owais6153@gmail.com>
2021-10-01 02:09:38 +0500

    New migration

--- app/Http/Controllers/front/AboutController.php
+++ app/Http/Controllers/front/AboutController.php
@@ -23,6 +23,7 @@ class AboutController extends Controller
         $getabout = About::where('id','=','1')->first();
         $getdata=User::select('currency')->where('type','1')->first();
         $getcategory = Category::where('is_available','1')->where('is_deleted','2')->get();
+        $user_id = Session::get('id');
          if (Session::get('id')) {
             $cartdata=Cart::with('itemimage')->select('id','qty','price','item_notes','cart.variation','item_name','tax',\DB::raw("CONCAT('".url('/storage/app/public/images/item/')."/', item_image) AS item_image"),'item_id','addons_id','addons_name','addons_price')
             ->where('user_id',$user_id)
