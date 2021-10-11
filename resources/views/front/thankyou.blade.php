93f3155baf8b0d8df8f84da30d4f1b927d507fe9
osamageeksroot <osama.arshad@geeksroot.com>
2021-10-12 02:58:52 +0500

    thankyou page

--- resources/views/front/thankyou.blade.php
+++ resources/views/front/thankyou.blade.php
@@ -2,12 +2,18 @@
 @include('front.theme.header')
 
 
-
-<h1 style="
-    margin-top: 200px;
-    font-size: 60px;
-    text-align: center;
-">Thankyou</h1>
+<section class="sec-thankyou">
+	<div class="container">
+		<div class="thankyou-box">
+			<div class="thanku-img">
+			<img src='{!! asset("storage/app/public/assets/images/Group-1171.png") !!}' alt="">
+			</div>
+			<h1>Thankyou</h1>
+			<p>We will contact you as soon as possible.</p>
+			
+		</div>
+	</div>
+</section>
 
 
 @include('front.theme.footer')
